ff85cbf5640b3a8c86aa3a157f83bf9aef99fbf4
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2022-03-02 06:26:01 -0500

    reorganizing of lists works

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,3 +1,4 @@
+import { arrayRemove, arrayUnion } from "firebase/firestore";
 import { useAuth } from "lib/AuthUserContext";
 import {
   addFirestoreDoc,
@@ -19,48 +20,99 @@ const Dashboard = () => {
   const [listName, setListName] = useState("");
   const [loading, setLoading] = useState(false);
   const [gallery, setGallery] = useState(null);
+  const [mouseLocation, setMouseLocation] = useState({
+    left: 0,
+    top: 0,
+  });
+  const [currentDrag, setCurrentDrag] = useState({
+    homeRecipe: null,
+    homeList: null,
+    destinationList: null,
+  });
 
   useEffect(() => {
     if (authUser === "null") router.push("/");
   }, [authUser, router]);
+  const populateGallery = async () => {
+    const list = await getSingleFirestoreDoc(
+      COLLECTION_NAMES.RECIPE_LISTS,
+      authUser.uid
+    );
+    if (!list || list.error) setGallery([]);
+    const lists = Object.getOwnPropertyNames(list);
+    const dbLists = (
+      await Promise.all(
+        lists
+          .map(async (i) => {
+            if (!(typeof list[i] === "object")) return;
+            const recps = await Promise.all(
+              list[i]?.list?.map(async (id) => {
+                const data = await getSingleFirestoreDoc(
+                  COLLECTION_NAMES.RECIPE_DATA,
+                  id
+                );
+                console.log(data, "data");
+                return data && !data.error ? { ...data, id } : null;
+              })
+            );
+            console.log(recps, "recps");
+            const fin = {
+              listName: list[i]?.name,
+              recipes: recps || [],
+              id: i,
+              date: list[i]?.date,
+            };
+            return fin;
+          })
+          .filter((val) => !!val)
+      )
+    )
+      .filter((val) => !!val)
+      .sort((a, b) => (a.id === "All_Recipes" ? 1 : a.date > b.date ? 1 : -1));
+    console.log(dbLists, "LISTTSS");
+    setGallery(dbLists);
+  };
   useEffect(() => {
-    const populateGallery = async () => {
-      const list = await getSingleFirestoreDoc(
-        COLLECTION_NAMES.RECIPE_LISTS,
-        authUser.uid
-      );
-      if (!list || list.error) setGallery([]);
-      const lists = Object.keys(list);
-      const dbLists = (
-        await Promise.all(
-          lists
-            .map(async (i) => {
-              if (!(typeof list[i] === "object")) return;
-              const recps = await Promise.all(
-                list[i]?.list?.map(async (id) => {
-                  const data = await getSingleFirestoreDoc(
-                    COLLECTION_NAMES.RECIPE_DATA,
-                    id
-                  );
-                  console.log(data, "data");
-                  return data && !data.error ? { ...data, id } : null;
-                })
-              );
-              console.log(recps, "recps");
-              const fin = {
-                listName: list[i]?.name,
-                recipes: recps || [],
-              };
-              return fin;
-            })
-            .filter((val) => !!val)
-        )
-      ).filter((val) => !!val);
-      setGallery(dbLists);
-    };
     if (gallery === null && authUser) populateGallery();
     console.log(gallery, authUser);
-  }, [gallery]);
+  }, [authUser]);
+  useEffect(() => {
+    console.log(currentDrag);
+  }, [currentDrag]);
+
+  const changeGrouping = async () => {
+    if (
+      (
+        await updateFirestoreDoc(
+          {
+            [`${currentDrag.destinationList.id}.list`]: arrayUnion(
+              currentDrag.homeRecipe.id
+            ),
+          },
+          COLLECTION_NAMES.RECIPE_LISTS,
+          authUser.uid
+        )
+      ).error
+    )
+      return false;
+    else if (
+      (
+        await updateFirestoreDoc(
+          {
+            [`${currentDrag.homeList.id}.list`]: arrayRemove(
+              currentDrag.homeRecipe.id
+            ),
+          },
+          COLLECTION_NAMES.RECIPE_LISTS,
+          authUser.uid
+        )
+      ).error
+    )
+      return false;
+    populateGallery();
+    return true;
+  };
+
   return (
     <div className={styles.main}>
       <h1>Dashboard</h1>
@@ -93,7 +145,7 @@ const Dashboard = () => {
             if (
               (!lists ||
                 lists.error ||
-                Object.keys(lists).find(
+                Object.getOwnPropertyNames(lists).find(
                   (val) => lists[val]?.name == listName
                 )) &&
               typeof window !== "undefined"
@@ -108,12 +160,13 @@ const Dashboard = () => {
                 [listName.replace(/\s+/g, "")]: {
                   list: [],
                   name: listName,
+                  date: new Date(),
                 },
               },
               COLLECTION_NAMES.RECIPE_LISTS,
               authUser.uid
             );
-            setGallery(null);
+            populateGallery();
             setLoading(false);
           }}
         >
@@ -123,14 +176,66 @@ const Dashboard = () => {
       <div className={styles.gallery}>
         {gallery &&
           gallery.map((list, i) => (
-            <div key={list.listName} className={styles.row}>
-              <h2>{list.listName && list.listName}</h2>
+            <div
+              key={list.listName}
+              className={styles.row}
+              onDragEnter={(e) => {
+                e.stopPropagation();
+                if (currentDrag.homeRecipe) {
+                  setCurrentDrag({
+                    ...currentDrag,
+                    destinationList: list,
+                  });
+                }
+              }}
+            >
+              <span className={styles.listTitle}>
+                <h2>{list.listName && list.listName}</h2>
+                <Button
+                  className={styles.groceryButton}
+                  onClick={() => {
+                    router.push(`/list/${list.id}`);
+                  }}
+                >
+                  Turn this into a grocery list!
+                </Button>
+              </span>
+              <hr />
               <div className={styles.rowContainer}>
                 {list.recipes.map((recipe) => (
                   <Link key={recipe.name} href={`/view/${recipe.id}`}>
-                    <a className={styles.recipe}>
+                    <a
+                      className={styles.recipe}
+                      onDragStart={(e) => {
+                        e.stopPropagation();
+                        setCurrentDrag({
+                          ...currentDrag,
+                          homeRecipe: recipe,
+                          homeList: list,
+                        });
+                      }}
+                      onDragEnd={async (e) => {
+                        e.stopPropagation();
+                        if (!currentDrag.destinationList) {
+                          setCurrentDrag({
+                            homeRecipe: null,
+                            homeList: null,
+                            destinationList: null,
+                          });
+                        } else {
+                          await changeGrouping();
+                          setCurrentDrag({
+                            homeRecipe: null,
+                            homeList: null,
+                            destinationList: null,
+                          });
+                        }
+                      }}
+                    >
                       <h3>{recipe.name}</h3>
-                      {recipe.image && <img src={recipe.image} />}
+                      {recipe.image && (
+                        <img src={recipe.image} alt={recipe.name} />
+                      )}
                     </a>
                   </Link>
                 ))}
--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,11 @@
 import styles from "styles/Button.module.css";
 
-const Button = ({ onClick, children, ...otherClasses }) => (
-  <button className={styles.button} onClick={onClick} {...otherClasses}>
+const Button = ({ onClick, children, className, ...otherClasses }) => (
+  <button
+    className={`${styles.button} ${className}`}
+    onClick={onClick}
+    {...otherClasses}
+  >
     {children}
   </button>
 );
